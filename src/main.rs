b6b7f0a7ff2db9f6bc69bab12fee8b6297590edc
0xcy <993921@qq.com>
2024-12-29 09:56:40 +0800

    support monitoring ip

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(app_state.clone()))
             .configure(api::config_services)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
